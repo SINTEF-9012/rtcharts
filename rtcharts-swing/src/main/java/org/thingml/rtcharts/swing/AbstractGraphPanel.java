ea278e3c9fa2a6dc14ed58cd88b979968eb8d356
Franck Fleurey <franck@fleurey.com>
2012-09-18 17:39:46 +0200

    fix header plugin

--- rtcharts-swing/src/main/java/org/thingml/rtcharts/swing/AbstractGraphPanel.java
+++ rtcharts-swing/src/main/java/org/thingml/rtcharts/swing/AbstractGraphPanel.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * ../HEADER
+ */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
